18c7456a4b03d4db68315889426b8a20f3374e24
Basil1k <142823362+Basil1k@users.noreply.github.com>
2026-07-12 03:06:55 +0500

    work on Makefile

--- algoritms.c
+++ algoritms.c
@@ -2,5 +2,20 @@
 
 int main(char** argv, int argc) {
   printf("========GROKKING ALGORITMS========");
+  test_binay_search();
   return 0;
+}
+
+void test_binay_search() {
+  int arr[10] = {-2, 0, 3, 8, 11, 19, 23, 39, 45, 97};
+  int target = 45;
+
+  printf("Array: ");
+  for (int i = 0; i < 10; i++) {
+    printf("%i, ", arr[i]);
+  }
+  printf("\nSearch for: %i\n", target);
+  int* result = binary_search_int(&arr, 10, target);
+
+  printf();
 }
--- algoritms.h
+++ algoritms.h
@@ -1,7 +1,10 @@
 #ifndef ALGORIMS
 #define ALGORIMS
 
+#include <stdlib.h>
+
 #include "binary_search.h"
 
+void test_binay_search();
 
 #endif
